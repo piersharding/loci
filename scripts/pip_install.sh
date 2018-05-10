071966dfae516d2c216f9c5bf5bbc62ba39aaea2
Piers Harding <piers@catalyst.net.nz>
2018-05-10 18:34:47 +1200

    try something new

--- scripts/pip_install.sh
+++ scripts/pip_install.sh
@@ -4,4 +4,7 @@ set -ex
 
 packages=$@
 
+# ram this one home as it break elsewhere
+pip install enum-compat
+
 pip install --no-cache-dir --only-binary :all: --no-compile -c /tmp/wheels/upper-constraints.txt --find-links /tmp/wheels/ ${PIP_ARGS} ${packages}
